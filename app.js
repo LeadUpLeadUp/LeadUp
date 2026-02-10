8be56c01dec4a9bd46ca4fad6d9eb19e31c59056
LeadUpLeadUp <ors35161@gmail.com>
2026-02-10 12:10:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
   // 1) Publish your Apps Script as Web App (Anyone with the link).
   // 2) Paste the /exec URL here. Example:
   // const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/XXXX/exec';
-  const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxFLUS4K2zSjVC-3TvLOOeRb6JyiwNmlUcG6d753qfAncLlbEgtKUgGsfS3iGoJJMNxMw/exec';
+ const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbznkLG4jzYBbJ29PF4MO2zopC4azqCWTN7LInPUQoA74rwOWwgBDNUrerWxsfC4zwaSfA/exec";
+
 
 
   // If true, the app will try to auto-connect on load (ping).
