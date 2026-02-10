42638656f6569b70a1525d4d15292f667c3e7bfd
LeadUpLeadUp <ors35161@gmail.com>
2026-02-10 16:08:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 /* ===== GOOGLE SHEETS CONNECTION ===== */
-
-const GOOGLE_SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbznkLG4jzYBbJ29PF4MO2zopC4azqCWTN7LInPUQoA74rwOWwgBDNUrerWxsfC4zwaSfA/exec";
-
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyhj4z2KPlhG0eMvFW3CL9aogdxq02oQgwVZkmVcXIjgzLkyw7lYzIfdJlB7Hi8ogmWZg/exec";
 console.log("Sheets URL:", GOOGLE_SCRIPT_URL);
 
 /* ===== API CALL ===== */
