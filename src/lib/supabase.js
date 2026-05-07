26faa4a36c7cc2bd74219f82af3121a087068595
LeadUpLeadUp <ors35161@gmail.com>
2026-05-07 18:46:51 +0300

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://vhvlkerectggovfihjgm.supabase.co'
-const SUPABASE_ANON = 'sb_publishable_JixJJelGPWcP0BPKGq96Lw_nIiMyIBb'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
