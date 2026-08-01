c176b6e36ebcb37eef2e780a357842714ba70c88
LeadUpLeadUp <ors35161@gmail.com>
2026-08-01 11:33:44 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
    לאיפוס ידני: כפתור "החל עדכון" במערכת כבר מוחק את כל המטמונים ומבטל רישום SW.
 */
 
-const CACHE_VERSION = "gi-v1-20260801-settings";
+const CACHE_VERSION = "gi-v1-20260801-p1-crosscut";
 const RUNTIME_CACHE = `gi-runtime-${CACHE_VERSION}`;
 
 // סיומות שמותר להגיש מהמטמון.
